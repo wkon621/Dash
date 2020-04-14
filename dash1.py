d94100816ca496da4b5fa5bfec9a6c17c1e07234
wkon621 <59033339+wkon621@users.noreply.github.com>
2020-04-14 17:15:55 -0400

    Add files via upload

---
+++ dash1.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 17:13:16 2020
+
+@author: IAN
+"""
+
+import pandas as pd
+from pandas import DataFrame
+import plotly
+import plotly.graph_objs as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+newlocationsdf = pd.read_csv('sarscov2-output.csv')
+newlocationsdf
+
+newlocationsdf['text'] = newlocationsdf['province'] + '<br>' + newlocationsdf['country'] + '<br>' + newlocationsdf['confirmed'].astype(str)
+                                                                                                                  
+fig = go.Figure(data=go.Scattergeo(
+        locationmode = 'country names',
+        lon = newlocationsdf['longitude'],
+        lat = newlocationsdf['latitude'],
+        text = newlocationsdf['text'],
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            opacity = 0.8,
+            reversescale = False,
+            autocolorscale = False,
+            symbol = 'square',
+            line = dict(
+                width=1,
+                color='rgba(102, 102, 102)'
+            ),
+            colorscale = 'Reds',
+            cmin = newlocationsdf['confirmed'].min(),
+            color = newlocationsdf['confirmed'],
+            cmax = newlocationsdf['confirmed'].max(),
+            colorbar_title="Total Number Confirmed Cases"
+        )))
+
+fig.update_layout(
+        title = 'Countries Affected by SARS-COV-2',
+        geo = dict(
+            scope='world',
+            projection_type='natural earth',
+            showland = True,
+            landcolor = "rgb(250, 250, 250)",
+            subunitcolor = "rgb(217, 217, 217)",
+            countrycolor = "rgb(217, 217, 217)",
+            countrywidth = 0.5,
+            subunitwidth = 0.5
+        ),
+    )
+#fig.show()
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+app.run_server(debug=False)  # Turn off reloader if inside Jupyter
